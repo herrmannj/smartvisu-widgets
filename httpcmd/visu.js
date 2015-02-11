ffc9c3ab8284cd4c6460b05b669a7d318b80c120
Bernd Gewehr <bgewehr@users.noreply.github.com>
2015-02-11 20:33:51 +0100

    Update visu.js

--- httpcmd/visu.js
+++ httpcmd/visu.js
@@ -1,1 +1,6 @@
-/** nothing needed */
+// ----- basic.httpcmd ----------------------------------------------------------
+$(document).delegate('a[data-widget="basic.httpcmd"]', {
+	'click': function (event) {
+		$.ajax($(this).attr('data-val'));
+	}
+});
