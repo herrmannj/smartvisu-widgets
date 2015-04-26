3b4b6827f6a9b4e65893b7966a6c16ad8397909e
Bernd Gewehr <bgewehr@users.noreply.github.com>
2015-04-26 15:21:34 +0200

    Update widget_selectmenu.js

--- selectmenu/widget_selectmenu.js
+++ selectmenu/widget_selectmenu.js
@@ -1,8 +1,7 @@
 	// ----- basic.select_single -------------------------------------------------------
 $(document).delegate('select[data-widget="basic.selectmenu"]', {
 	'update': function (event, response) {
-		var prog = response[0].match(/prog[123]/g);
-		$(this).val(prog).selectmenu('refresh');
+		$(this).val(response[0]).selectmenu('refresh');
 		// DEBUG: console.log("[basic.selectmenu] update '" + this.id + "': aktuell: " +  $(this).attr('selected'), response); 
 	},
 
